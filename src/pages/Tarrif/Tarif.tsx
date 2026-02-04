31d3563350d1fbf8b0c19e710866a75a6a7a33cd
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2026-02-04 11:24:13 +0100

    update

--- src/pages/Tarrif/Tarif.tsx
+++ src/pages/Tarrif/Tarif.tsx
@@ -92,21 +92,21 @@ const DynamicForm: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center min-h-screen py-12 bg-slate-50">
-      {/* MODAL */}
+    <div className="flex justify-center min-h-screen py-12 ">
+      {/* MODAL. */}
       {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
-          <div className="bg-white p-6 rounded-xl shadow-lg w-full max-w-lg">
-            <h2 className="text-xl font-semibold text-sky-700 mb-4">Informations</h2>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40">
+          <div className="w-full max-w-lg p-6 bg-white shadow-lg rounded-xl">
+            <h2 className="mb-4 text-xl font-semibold text-sky-700">Informations</h2>
 
-            <pre className="whitespace-pre-wrap text-gray-800 mb-4">
+            <pre className="mb-4 text-gray-800 whitespace-pre-wrap">
               {modalText}
             </pre>
 
             <div className="flex justify-end">
               <button
                 onClick={() => setShowModal(false)}
-                className="px-4 py-2 bg-sky-600 text-white rounded-lg hover:bg-sky-700"
+                className="px-4 py-2 text-white rounded-lg bg-sky-600 hover:bg-sky-700"
               >
                 Fermer
               </button>
